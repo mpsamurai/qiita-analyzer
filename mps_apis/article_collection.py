2c9b8aab3580ed165c110e4e8b9be5c485fb75d1
hiroshi teraoka <hiroshi4000@gmail.com>
2017-08-16 09:31:23 +0900

    qiitaアクセストークンを分離

--- mps_apis/article_collection.py
+++ mps_apis/article_collection.py
@@ -7,67 +7,57 @@ from qiita_v2.client import QiitaClient
 
 import django
 from django.conf import settings
-
+import credentials  # <<<<<< アクセストークンは外部にはださないこと
 django.setup()
 from qiita.models import Article
-# qiitaの個人用アクセストークン
-TOKEN = "4eb33e0057f314d4e8cdcc973a906325a24438fa"
 
-class child_QiitaClient(QiitaClient):
+
+client = QiitaClient(access_token=credentials.TOKEN)
+id = 'JunyaKaneko'
+
+def get_particular_user_article(id):
     """
-    QiitaClientを継承
+    特定のユーザーの特定のタグに関する記事を取得
+    :param id: User_id
+    :return: タイトル,url,投稿日,更新日,本文 
     """
-    def get_particular_user_article(self, id):
-        """
-        特定のユーザーの特定のタグに関する記事を取得
-        :param id: User_id
-        :return: タイトル,url,投稿日,更新日,本文 
-        """
-        # 特定のユーザーの投稿記事一覧を取得
-        res_user = self.list_user_items(id)
-        article_list = res_user.to_json()
-        try:  # その中でも特定のタグ『Python』で取得
-            for article in article_list:
-                for tags in article['tags']:
-                    if tags['name'] == 'Python':
-                        # タイトル取得
-                        title = article['title']
-                        # print("タイトル：%s" % article['title'])
-                # urlを取得
-                if article['url']:
-                    url = article['url']
-                    # print("url：%s" % article['url'])
-                # 投稿日
-                if article['created_at']:
-                    created_at = article['created_at']
-                    # print("投稿日：%s" % article['created_at'])
-                # 最新更新日
-                if article['updated_at']:
-                    updated_at = article['updated_at']
-                    # print("最新更新日：%s" % article['updated_at'])
-                # 本文(Markdownで取得)
-                if article['body']:
-                    # html_body = mistune.markdown(article['body'])
-                    article_body = article['body']
-                    # print("本文：%s" % article['body'])  # Markdownを取得
+    # 特定のユーザーの投稿記事一覧を取得
+    res_user = client.list_user_items(id)
+    article_list = res_user.to_json()
+    try:  # その中でも特定のタグ『Python』で取得
+        for article in article_list:
+            for tags in article['tags']:
+                if tags['name'] == 'Python':
+                    # タイトル取得
+                    title = article['title']
+                    # print("タイトル：%s" % article['title'])
+            # urlを取得
+            if article['url']:
+                url = article['url']
+                # print("url：%s" % article['url'])
+            # 投稿日
+            if article['created_at']:
+                created_at = article['created_at']
+                # print("投稿日：%s" % article['created_at'])
+            # 最新更新日
+            if article['updated_at']:
+                updated_at = article['updated_at']
+                # print("最新更新日：%s" % article['updated_at'])
+            # 本文(Markdownで取得)
+            if article['body']:
+                # html_body = mistune.markdown(article['body'])
+                article_body = article['body']
+                # print("本文：%s" % article['body'])  # Markdownを取得
 
-                # article = Article(title=title,
-                #                   url=url,
-                #                   created_at=created_at,
-                #                   updated_at=updated_at,
-                #                   article_body=article_body)
-                # article.save()
-                article_query = Article(title=title,
-                                        url=url,
-                                        article_body=article_body)
-                article_query.save()
-                print(Article.objects.all())
+            article_query = Article(title=title,
+                                    url=url,
+                                    article_body=article_body)
+            article_query.save()
+            print(Article.objects.all())
 
-                return title, url, created_at, updated_at, article_body
-        except KeyError:
-            pass
+            return title, url, created_at, updated_at, article_body
+    except KeyError:
+        pass
 
 
-if __name__ == '__main__':
-    client = child_QiitaClient(access_token=TOKEN)
-    print(client.get_particular_user_article('JunyaKaneko'))
+get_particular_user_article(id)
