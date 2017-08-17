fafc14670a472dad814d7adda075a738113cbbd5
hiroshi teraoka <hiroshi4000@gmail.com>
2017-08-17 21:57:59 +0900

    oauthを使ってQiitaのアクセストークン取得までできた

--- mps_apis/qiita/views.py
+++ mps_apis/qiita/views.py
@@ -5,6 +5,7 @@ from django.views import View
 # コマンドラインをスクリプトから実行
 import subprocess
 import urllib.request, urllib.parse
+import json
 
 
 
@@ -24,4 +25,24 @@ class UpdatesView(View):
 class RedirectView(View):
 
     def get(self, request):
-        return render(request, 'qiita/redirect.html', {})
+        if "code" in request.GET:
+            # query_paramが指定されている場合の処理( codeを取得 )
+            param_value = request.GET.get("code")
+
+            data = {
+                "client_id": "5ac39514be164adfe85e9df3defc7ca5e791adc9",
+                "client_secret": "196628b368ffddaa232725109e377b11d4093882",
+                "code": param_value
+            }
+            json_data = json.dumps(data).encode("utf-8")
+            url = "https://qiita.com/api/v2/access_tokens"
+            method = "POST"
+            headers = {"Content-Type": "application/json"}
+
+        req = urllib.request.Request(url, data=json_data, method=method, headers=headers)
+        with urllib.request.urlopen(req) as response:
+            response_body = response.read().decode("utf-8")
+        # print("param_value", param_value)
+        return render(request, 'qiita/redirect.html', {
+            'response_body': response_body
+        })
